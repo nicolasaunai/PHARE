189ebae90d95bd081463ec7b95461e08983be81f
UCaromel <caromelulysse@gmail.com>
2024-09-17 09:55:10 +0200

    preparing mhd usablevecfield

--- tests/core/data/vecfield/test_vecfield_fixtures_mhd.hpp
+++ tests/core/data/vecfield/test_vecfield_fixtures_mhd.hpp
@@ -3,13 +3,14 @@
 
 #include "tests/core/data/field/test_field_fixtures_mhd.hpp"
 #include "tests/core/data/tensorfield/test_tensorfield_fixtures_mhd.hpp"
-#include <core/mhd/mhd_quantities.hpp>
+#include "core/mhd/mhd_quantities.hpp"
+#include "core/data/vecfield/vecfield.hpp"
 
 namespace PHARE::core
 {
 
 template<std::size_t dim>
-using VecField_t = VecField<Field_t<dim>, MHDQuantity>;
+using VecFieldMHD = VecField<FieldMHD<dim>, MHDQuantity>;
 
 template<std::size_t dim>
 using UsableVecFieldMHD = UsableTensorFieldMHD<dim, /*rank=*/1>;
